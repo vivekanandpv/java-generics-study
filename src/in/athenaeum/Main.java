fea0567569724d6edc5d06646a00e8ee32bbcb0b
vivek <vivek@athenaeum.in>
2021-02-28 23:23:05 +0530

    done

--- src/in/athenaeum/Main.java
+++ src/in/athenaeum/Main.java
@@ -5,32 +5,13 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-        //  https://docs.oracle.com/en/java/javase/15/docs/api/java.base/java/util/List.html
-        List<Vehicle> vehiclesAsArrayList = new ArrayList<>();
-        List<Vehicle> vehiclesAsLinkedList = new LinkedList<>();
-        List<Vehicle> vehiclesAsVector = new Vector<>();    //  thread-safe, performance penalty
+        //  https://en.wikipedia.org/wiki/Generics_in_Java#Problems_with_type_erasure
 
-        //  https://docs.oracle.com/en/java/javase/15/docs/api/java.base/java/util/Set.html
-        Set<Vehicle> vehiclesAsHashSet = new HashSet<>();
-        Set<Vehicle> vehiclesAsLinkedHashSet = new LinkedHashSet<>();
+        //  Type parameter is erased in the runtime
+        List<Vehicle> vehicleList = new ArrayList<>();
+        List<String> stringList = new ArrayList<>();
 
-        //  https://docs.oracle.com/en/java/javase/15/docs/api/java.base/java/util/Map.html
-        Map<String, Vehicle> stringVehicleAsHashMap = new HashMap<>();
-        Map<String, Vehicle> stringVehicleAsLinkedHashMap = new LinkedHashMap<>();
-        Map<String, Vehicle> stringVehicleAsTreeMap = new TreeMap<>();
-
-        //  Self study:
-        //  https://docs.oracle.com/en/java/javase/15/docs/api/java.base/java/util/Collection.html
-        //  https://docs.oracle.com/en/java/javase/15/docs/api/java.base/java/util/Queue.html
-        //  https://docs.oracle.com/en/java/javase/15/docs/api/java.base/java/util/Deque.html
-        //  https://docs.oracle.com/en/java/javase/15/docs/api/java.base/java/util/SortedSet.html
-        //  https://docs.oracle.com/en/java/javase/15/docs/api/java.base/java/util/Stack.html
-
-        //  https://docs.oracle.com/en/java/javase/15/docs/api/java.base/java/util/NavigableMap.html
-        //  https://docs.oracle.com/en/java/javase/15/docs/api/java.base/java/util/SortedMap.html
-        //  https://docs.oracle.com/en/java/javase/15/docs/api/java.base/java/util/concurrent/ConcurrentMap.html
-        //  https://docs.oracle.com/en/java/javase/15/docs/api/java.base/java/util/concurrent/ConcurrentNavigableMap.html
-        //  https://docs.oracle.com/en/java/javase/15/docs/api/java.base/java/util/IdentityHashMap.html
-        //  https://docs.oracle.com/en/java/javase/15/docs/api/java.base/java/util/WeakHashMap.html
+        //  In the runtime, it's just List (of Object)
+        System.out.println(vehicleList.getClass() + " = " + stringList.getClass());
     }
 }
