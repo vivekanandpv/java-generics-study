22b7347179008dc100d109dd2f3b1a7d6a008c85
vivek <vivek@athenaeum.in>
2021-02-28 23:13:01 +0530

    done

--- src/in/athenaeum/Main.java
+++ src/in/athenaeum/Main.java
@@ -1,24 +1,36 @@
 package in.athenaeum;
 
+import java.util.*;
+
 public class Main {
 
     public static void main(String[] args) {
-        //  Regular polymorphism for the contained element
-	    Container<Vehicle> vehicleContainerForVehicle
-                = new Container<>(new Vehicle());
+        //  https://docs.oracle.com/en/java/javase/15/docs/api/java.base/java/util/List.html
+        List<Vehicle> vehiclesAsArrayList = new ArrayList<>();
+        List<Vehicle> vehiclesAsLinkedList = new LinkedList<>();
+        List<Vehicle> vehiclesAsVector = new Vector<>();    //  thread-safe, performance penalty
 
-	    Container<Vehicle> vehicleContainerForCar
-                = new Container<>(new Car());
+        //  https://docs.oracle.com/en/java/javase/15/docs/api/java.base/java/util/Set.html
+        Set<Vehicle> vehiclesAsHashSet = new HashSet<>();
+        Set<Vehicle> vehiclesAsLinkedHashSet = new LinkedHashSet<>();
 
-	    Container<Vehicle> vehicleContainerForTruck
-                = new Container<>(new Truck());
+        //  https://docs.oracle.com/en/java/javase/15/docs/api/java.base/java/util/Map.html
+        Map<String, Vehicle> stringVehicleAsHashMap = new HashMap<>();
+        Map<String, Vehicle> stringVehicleAsLinkedHashMap = new LinkedHashMap<>();
+        Map<String, Vehicle> stringVehicleAsTreeMap = new TreeMap<>();
 
-	    Container<Vehicle> vehicleContainerForSportsCar
-                = new Container<>(new SportsCar());
+        //  Self study:
+        //  https://docs.oracle.com/en/java/javase/15/docs/api/java.base/java/util/Collection.html
+        //  https://docs.oracle.com/en/java/javase/15/docs/api/java.base/java/util/Queue.html
+        //  https://docs.oracle.com/en/java/javase/15/docs/api/java.base/java/util/Deque.html
+        //  https://docs.oracle.com/en/java/javase/15/docs/api/java.base/java/util/SortedSet.html
+        //  https://docs.oracle.com/en/java/javase/15/docs/api/java.base/java/util/Stack.html
 
-        System.out.println(vehicleContainerForVehicle.getElement().getClass().getName());
-        System.out.println(vehicleContainerForCar.getElement().getClass().getName());
-        System.out.println(vehicleContainerForTruck.getElement().getClass().getName());
-        System.out.println(vehicleContainerForSportsCar.getElement().getClass().getName());
+        //  https://docs.oracle.com/en/java/javase/15/docs/api/java.base/java/util/NavigableMap.html
+        //  https://docs.oracle.com/en/java/javase/15/docs/api/java.base/java/util/SortedMap.html
+        //  https://docs.oracle.com/en/java/javase/15/docs/api/java.base/java/util/concurrent/ConcurrentMap.html
+        //  https://docs.oracle.com/en/java/javase/15/docs/api/java.base/java/util/concurrent/ConcurrentNavigableMap.html
+        //  https://docs.oracle.com/en/java/javase/15/docs/api/java.base/java/util/IdentityHashMap.html
+        //  https://docs.oracle.com/en/java/javase/15/docs/api/java.base/java/util/WeakHashMap.html
     }
 }
