92a469d935019452d72aed1b1e05e520685af7eb
vivek <vivek@athenaeum.in>
2021-03-01 12:33:05 +0530

    done

--- src/in/athenaeum/Main.java
+++ src/in/athenaeum/Main.java
@@ -5,20 +5,32 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-        Container<Vehicle> vehicleContainer = new Container<>();
+        List<Car> cars = List.of(new SportsCar(), new Car());
+        List<Vehicle> vehicles = new ArrayList<>();
+        vehicles.add(new Vehicle());
+        vehicles.add(new Vehicle());
 
-        List<Vehicle> vehicleList = List.of(new Vehicle(), new Truck(), new Car(), new SportsCar());
-        List<Truck> truckList = List.of(new Truck(), new Truck());
-        List<Car> carList = List.of(new Car(), new Car());
-        List<SportsCar> sportsCarList = List.of(new SportsCar(), new SportsCar());
+        copy(cars, vehicles);
 
-        vehicleContainer.addCollection(sportsCarList);
-        vehicleContainer.addCollection(carList);
-        vehicleContainer.addCollection(truckList);
-        vehicleContainer.addCollection(vehicleList);
-
-        for(Vehicle v: vehicleContainer) {
+        for(Vehicle v: vehicles) {
             System.out.println(v.getClass());
+        }
+    }
+
+    //  Type parameters such as <E> should be declared upfront for methods
+    //  A non-generic class such as Main can have a generic method such as copy
+    //  Similarly a generic class may have a non-generic method
+
+    //  EG;SP: Extends for get operations; Super for put operations
+    //  Both cannot be applied simultaneously.
+    //  Use exact type parameter when both get and put are needed.
+    public static <E> void copy(List<? extends E> source, List<? super E> target) {
+        if (source.size() > target.size()) {
+            throw new RuntimeException("Target is shorter than source");
+        }
+
+        for(int i = 0; i < source.size(); ++i) {
+            target.set(i, source.get(i));
         }
     }
 }
